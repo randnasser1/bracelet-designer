f47c38d8a12581b322795450dc23028ddafb5290
Rand Mansour <randn.mansour@gmail.com>
2025-06-04 18:51:34 +0300

    Create charms.js

---
+++ charms.js
@@ -0,0 +1,97 @@
+// Charm image lists with categories and sold out status
+const specialCharms = [
+  // 8 Ball charms
+  { src: 'special/8ball/38.png', category: '8ball', soldOut: false },
+  { src: 'special/8ball/39.png', category: '8ball', soldOut: false },
+  { src: 'special/8ball/40.png', category: '8ball', soldOut: false },
+  { src: 'special/8ball/41.png', category: '8ball', soldOut: false },
+  { src: 'special/8ball/42.png', category: '8ball', soldOut: false },
+  { src: 'special/8ball/43.png', category: '8ball', soldOut: false },
+  { src: 'special/8ball/44.png', category: '8ball', soldOut: false },
+  { src: 'special/8ball/45.png', category: '8ball', soldOut: false },
+  { src: 'special/8ball/46.png', category: '8ball', soldOut: false },
+  { src: 'special/8ball/48.png', category: '8ball', soldOut: false },
+  { src: 'special/8ball/49.png', category: '8ball', soldOut: false },
+  { src: 'special/8ball/50.png', category: '8ball', soldOut: false },
+
+  // Beach charms
+  { src: 'special/beach/beach.png', category: 'beach', soldOut: false },
+  { src: 'special/beach/beach1.png', category: 'beach', soldOut: false },
+  // ... rest of beach charms ...
+
+  // Bows charms
+  { src: 'special/bows/beige.png', category: 'bows', soldOut: false },
+  { src: 'special/bows/black.png', category: 'bows', soldOut: false },
+  // ... rest of bows charms ...
+
+  // Cute and Simple charms
+  { src: 'special/cute and simple/1.png', category: 'simple', soldOut: false },
+  { src: 'special/cute and simple/2.png', category: 'simple', soldOut: false },
+  // ... rest of cute and simple charms ...
+
+  // Cute Specials charms
+  { src: 'special/cute specials/so cute.png', category: 'cutespecials', soldOut: false },
+  { src: 'special/cute specials/special girly.png', category: 'cute', soldOut: false },
+  // ... rest of cute specials charms ...
+
+  // Cute charms
+  { src: 'special/cute/cute.png', category: 'cute', soldOut: false },
+  { src: 'special/cute/cute2.png', category: 'cute', soldOut: false },
+  // ... rest of cute charms ...
+
+  // Mushrooms charms
+  { src: 'special/mushrooms/mushroom.png', category: 'mushrooms', soldOut: false },
+  { src: 'special/mushrooms/mushroom1.png', category: 'mushrooms', soldOut: false },
+  // ... rest of mushroom charms ...
+
+  // Red charms
+  { src: 'special/red/red.png', category: 'red', soldOut: false },
+  { src: 'special/red/red1.png', category: 'red', soldOut: false },
+  // ... rest of red charms ...
+
+  // Teddy charms
+  { src: 'special/teddy/teddy.png', category: 'teddy', soldOut: false },
+  { src: 'special/teddy/teddy2.png', category: 'teddy', soldOut: false }
+  // ... rest of teddy charms ...
+];
+
+const rareCharms = [
+  // Dangly charms
+  { src: 'rares/dangly/dangly.png', category: 'dangly', soldOut: false },
+
+  // Disney charms
+  { src: 'rares/disney/bluemonster.png', category: 'disney', soldOut: false },
+  { src: 'rares/disney/bunny.png', category: 'disney', soldOut: false },
+  // ... rest of disney charms ...
+
+  // Gold charms
+  { src: 'rares/gold/gold.png', category: 'gold', soldOut: false },
+  { src: 'rares/gold/gold2.png', category: 'gold', soldOut: false },
+  // ... rest of gold charms ...
+
+  // Sanrio charms
+  { src: 'rares/sanrio/51.png', category: 'sanrio', soldOut: false },
+  { src: 'rares/sanrio/51-gold.png', category: 'sanrio', soldOut: false },
+  { src: 'rares/sanrio/52.png', category: 'sanrio', soldOut: false },
+  { src: 'rares/sanrio/52-gold.png', category: 'sanrio', soldOut: false },
+  { src: 'rares/sanrio/53.png', category: 'sanrio', soldOut: false },
+  { src: 'rares/sanrio/53-gold.png', category: 'sanrio', soldOut: false },
+  { src: 'rares/sanrio/54.png', category: 'sanrio', soldOut: false },
+  { src: 'rares/sanrio/54-gold.png', category: 'sanrio', soldOut: false },
+  { src: 'rares/sanrio/55.png', category: 'sanrio', soldOut: false },
+  { src: 'rares/sanrio/55-gold.png', category: 'sanrio', soldOut: false },
+  { src: 'rares/sanrio/56.png', category: 'sanrio', soldOut: false },
+  { src: 'rares/sanrio/56-gold.png', category: 'sanrio', soldOut: false },
+  { src: 'rares/sanrio/57.png', category: 'sanrio', soldOut: false },
+  { src: 'rares/sanrio/57-gold.png', category: 'sanrio', soldOut: false },
+  { src: 'rares/sanrio/58.png', category: 'sanrio', soldOut: false },
+  { src: 'rares/sanrio/58-gold.png', category: 'sanrio', soldOut: false },
+  { src: 'rares/sanrio/59.png', category: 'sanrio', soldOut: false },
+  { src: 'rares/sanrio/59-gold.png', category: 'sanrio', soldOut: false },
+  { src: 'rares/sanrio/60.png', category: 'sanrio', soldOut: false },
+  { src: 'rares/sanrio/60-gold.png', category: 'sanrio', soldOut: false },
+  { src: 'rares/sanrio/61.png', category: 'sanrio', soldOut: false },
+  { src: 'rares/sanrio/61-gold.png', category: 'sanrio', soldOut: false },
+  { src: 'rares/sanrio/62.png', category: 'sanrio', soldOut: false },
+  { src: 'rares/sanrio/metalkitty.png', category: 'sanrio', soldOut: false }
+]; 
