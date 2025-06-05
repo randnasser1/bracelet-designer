dd3c53c64d5f27f4eb3022b25e91b081409d9c10
Rand Mansour <randn.mansour@gmail.com>
2025-06-05 23:40:59 +0300

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+// Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyD_iOEwu71dL4Lmfl7Km8lSlYFzSuubbzY",
+    authDomain: "italian-charms.firebaseapp.com",
+    projectId: "italian-charms",
+    storageBucket: "italian-charms.appspot.com",
+    messagingSenderId: "156559643870",
+    appId: "1:156559643870:web:a14807a2a6d1761b71de4f"
+}; 
