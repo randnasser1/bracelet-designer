971bba5fd34f5c582286c87f3009b3701aa35f0d
Rand Mansour <randn.mansour@gmail.com>
2025-06-16 00:56:28 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -3,7 +3,7 @@ const firebaseConfig = {
     apiKey: "AIzaSyD_iOEwu71dL4Lmfl7Km8lSlYFzSuubbzY",
     authDomain: "italian-charms.firebaseapp.com",
     projectId: "italian-charms",
-    storageBucket: "italian-charms.appspot.com",
+  storageBucket: "italian-charms.firebasestorage.app" ,// ← MUST match your CORS-configured bucket
     messagingSenderId: "156559643870",
     appId: "1:156559643870:web:a14807a2a6d1761b71de4f"
 }; 
